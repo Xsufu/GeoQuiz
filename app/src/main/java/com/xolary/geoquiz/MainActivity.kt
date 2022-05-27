604530017ae56aee0afb1b331d1e4d23009a0e05
Xsufu <aluno.macarrao@gmail.com>
2022-05-27 20:58:26 +0700

    Рефакторинг кода

--- app/src/main/java/com/xolary/geoquiz/MainActivity.kt
+++ app/src/main/java/com/xolary/geoquiz/MainActivity.kt
@@ -2,12 +2,14 @@ package com.xolary.geoquiz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    //Инициализация переменных
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
 
@@ -15,15 +17,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Привязка элементов макета к переменным
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
 
+        //Прослушка на нажатие кнопки "true"
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+            var toast = Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT
+            )
+            toast.show()
+
         }
 
+        //Прослушка на нажатие кнопки "false"
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            var toast = Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT
+            )
+            toast.show()
         }
     }
 }
