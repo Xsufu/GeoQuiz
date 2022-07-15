31cdf20a331ee2cf80e0f05761a4a29bd19bc73d
Xsufu <aluno.macarrao@gmail.com>
2022-07-15 14:16:57 +0700

    Удалены неиспользующиеся билиотеки

--- app/src/main/java/com/xolary/geoquiz/MainActivity.kt
+++ app/src/main/java/com/xolary/geoquiz/MainActivity.kt
@@ -2,8 +2,6 @@ package com.xolary.geoquiz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
